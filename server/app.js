83e3bf6797c98e2a739dc47ee31291382e911fde
Majid Khorshidian <mkhorshidian72@gmail.com>
2023-02-10 18:27:18 +0330

    add ejs template engine for server side rendering

---
+++ server/app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const morgan = require("morgan");
+const session = require("express-session");
+const path = require('path');
+
+const app = express();
+
+app.use(
+  session({
+    secret: "session secret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(morgan("dev"));
+
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.status || 500;
+  let message = err.message || "Internal Server Error";
+
+  if (statusCode === 500) message = "Internal Server Error";
+
+  res.status(statusCode).json({ message });
+});
+
+const PORT = 8010;
+
+app.listen(PORT, () => console.info(`server listening on port ${PORT} ...`));
